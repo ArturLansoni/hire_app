3c78578508d65263ffe7e0cb333583751cf421fc
Artur Lansoni <artur.lansoni@levva.io>
2024-03-23 12:27:53 -0300

    fix: remove unused import

--- lib/features/home/domain/cubits/order_cubit.dart
+++ lib/features/home/domain/cubits/order_cubit.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hire_app/core/utils/async_state.dart';
 import 'package:hire_app/core/utils/common_failures.dart';
